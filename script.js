46270d390bd2331cd4a403fc8e96deb60089fe22
m1sho <popwizardarraki@gmail.com>
2023-09-06 00:46:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
-// JavaScript to handle the click event on subheadings and show/hide the project background
-
+// JavaScript code for handling project subheading click events
 document.addEventListener("DOMContentLoaded", function () {
-    const webDevSubheading = document.getElementById("web-development");
+    const webDevelopmentSubheading = document.getElementById("web-development");
     const appsSubheading = document.getElementById("apps");
-    const webDevProjectBackground = document.getElementById("web-dev-projects");
-    const appsProjectBackground = document.getElementById("apps-projects");
+    const webDevProjects = document.getElementById("web-dev-projects");
+    const appsProjects = document.getElementById("apps-projects");
 
-    // Function to toggle project background visibility
-    function toggleProjectBackground(projectBackground) {
-        if (projectBackground.style.display === "block") {
-            // Hide project background
-            projectBackground.style.display = "none";
-        } else {
-            // Show project background
-            projectBackground.style.display = "block";
-        }
-    }
+    // Hide all project lists initially
+    webDevProjects.style.display = "none";
+    appsProjects.style.display = "none";
 
-    // Event listener for Web Development subheading
-    webDevSubheading.addEventListener("click", function () {
-        toggleProjectBackground(webDevProjectBackground);
+    // Add click event listeners to subheadings
+    webDevelopmentSubheading.addEventListener("click", function () {
+        webDevProjects.style.display = "block";
+        appsProjects.style.display = "none";
     });
 
-    // Event listener for Apps subheading
     appsSubheading.addEventListener("click", function () {
-        toggleProjectBackground(appsProjectBackground);
+        webDevProjects.style.display = "none";
+        appsProjects.style.display = "block";
     });
-
-    // Initially hide project backgrounds
-    webDevProjectBackground.style.display = "none";
-    appsProjectBackground.style.display = "none";
 });
